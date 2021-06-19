5ae5ecba303180fb90f0acf43dee6d8a2ee2fef3
josejavier <82359964+josejaviersanahuja@users.noreply.github.com>
2021-06-19 14:27:17 +0100

    añadimos muchos gifs. audio funciona. falta estilo

--- src/pages/Pagina1/index.js
+++ src/pages/Pagina1/index.js
@@ -1,9 +1,177 @@
-import React from 'react'
+import React from "react";
 
 export default function Pagina1() {
-    return (
-        <div>
-            Pagina 1
-        </div>
-    )
+  return (
+    <>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"44%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/PK5CQPd6rCF3y"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="please"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"56%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/aAGI2KxxkG2AM"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="roar with friends"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"50%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/nuJaAg1hdI89y"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="dancing2"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"55%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/w87qIKJBwmffO"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="dancing1"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"52%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/CWaKpKNmHinsY"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="painting"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"56%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/xm5xWCAZlbCRW"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="whole squad"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"60%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/Ms2YW90Qnyv4I"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="father son"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"42%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/umBCKOohjIpNe"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="simba swinging timon and pumba"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"56%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/26tPs9BvmhEP3pZHq"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="zuka zama"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"56%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/KJGBfqmGVPsys"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="hakuna matata"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:"0",paddingBottom:"49%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/WBAeLSp7gwZb2"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="nala pushes simba"
+        ></iframe>
+      </div>
+      </div>
+      <div style={{position:"relative", width:"100%", height:"50vh"}}>
+      <div style={{width:"100%",height:0,paddingBottom:"54%",position:"absolute", top:0}}>
+        <iframe
+          src="https://giphy.com/embed/emGuZV7zIcEcU"
+          width="100%"
+          height="100%"
+          style={{position:"absolute"}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+          title="simba pulls nala to lake"
+        ></iframe>
+      </div>
+      </div>
+    </>
+  );
 }
+
