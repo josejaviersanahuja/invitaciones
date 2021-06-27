aea23b07e5bb782553bfe868af68543aba653a11
josejavier <82359964+josejaviersanahuja@users.noreply.github.com>
2021-06-27 18:29:59 +0100

    fullscreen updated

---
+++ src/index.js
@@ -0,0 +1,30 @@
+var elem = document.documentElement;
+
+/* Abrir en fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+  document.getElementById("open").style.display="none";
+  document.getElementById("close").style.display="block";
+  
+}
+
+/* Cerrar fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+  
+  document.getElementById("open").style.display="block";
+  document.getElementById("close").style.display="none";
+  
+}
